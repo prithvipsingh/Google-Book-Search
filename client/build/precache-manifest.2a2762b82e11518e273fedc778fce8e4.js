d716a5139a007ed77890e3fdc7d7b8cc42689ff6
prithvipsingh <ppsdeora@gmail.com>
2020-07-14 21:49:46 +1000

    updates

---
+++ client/build/precache-manifest.2a2762b82e11518e273fedc778fce8e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6319a8f790327b4cba161815f6b13255",
+    "url": "/index.html"
+  },
+  {
+    "revision": "857e2aff71c949d18b90",
+    "url": "/static/css/main.2a8b0628.chunk.css"
+  },
+  {
+    "revision": "3d7af2e3e33fa58c5c41",
+    "url": "/static/js/2.cfd4dc03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfd4dc03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857e2aff71c949d18b90",
+    "url": "/static/js/main.b3ffe6d4.chunk.js"
+  },
+  {
+    "revision": "c46c97987879c7eab527",
+    "url": "/static/js/runtime-main.8dd9c097.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes)
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
