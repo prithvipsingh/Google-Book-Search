aeb007cb9170a6d2868b485301615e95ebcea740
prithvipsingh <ppsdeora@gmail.com>
2020-07-15 22:16:34 +1000

    debugging files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes)
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
