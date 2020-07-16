c60cf254dd540a7f77f3213d849e5a7412bdc6f1
prithvipsingh <ppsdeora@gmail.com>
2020-07-16 21:13:28 +1000

    run npm build

---
+++ client/build/precache-manifest.e3ea97a48542e9ff6097f4ac22ec5597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dba07bf540a430f30fcdffe645d3660",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1144c783309a17a2a77b",
+    "url": "/static/css/main.1a23aca4.chunk.css"
+  },
+  {
+    "revision": "3d7af2e3e33fa58c5c41",
+    "url": "/static/js/2.cfd4dc03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfd4dc03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1144c783309a17a2a77b",
+    "url": "/static/js/main.b3ffe6d4.chunk.js"
+  },
+  {
+    "revision": "c46c97987879c7eab527",
+    "url": "/static/js/runtime-main.8dd9c097.js"
+  },
+  {
+    "revision": "809949fcda59df7c8b321d9f8802d050",
+    "url": "/static/media/book.809949fc.png"
+  }
+]);
